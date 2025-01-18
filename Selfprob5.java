78066e29bb1cd24eef92ecb26f0e24ff113bb075
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-19 00:28:09 +0530

    committed

---
+++ Selfprob5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Selfprob5{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Kilometers"); 
+		double k=sc.nextInt();
+		double m=(k*0.621371);
+		System.out.println("In miles is "+m);
+	}
+}
