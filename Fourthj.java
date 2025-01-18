f8cce094856fc9a4f4e58bba42f4fe901110cfd1
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-18 23:57:26 +0530

    committed

---
+++ Fourthj.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Fourthj{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter radius"); 
+		double r=sc.nextInt();
+		double a=3.14*r*r;
+		System.out.println("Area of circle is "+a);
+	}
+}
