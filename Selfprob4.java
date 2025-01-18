073984ff661daf7e0bf739d5c20a1414266ce5af
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-19 00:22:20 +0530

    committed

---
+++ Selfprob4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Selfprob4{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter first"); 
+		int a=sc.nextInt();
+		System.out.println("Enter second");
+		int b=sc.nextInt();
+		System.out.println("Enter third");
+		int c=sc.nextInt();
+		double av=(a+b+c)/3;
+		System.out.println("Average is "+av);
+	}
+}
