5be88e03b6cbd883f25ff91bae4a65a0655062d2
PixelPirate07 <pandeydhairya02@gmail.com>
2025-01-18 23:20:57 +0530

    committed

---
+++ Secondj.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Secondj{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		System.out.println("Sum is "+(a+b));
+	}
+}
